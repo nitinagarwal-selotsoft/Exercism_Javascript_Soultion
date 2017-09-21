c1e914fdefa77fa652508ecef1666239592ca293
nitinagarwal-selotsoft <nitina@selotsoft.com>
2017-09-21 14:32:57 +0530

    21-09-2017

--- prime-factors/prime-factors.js
+++ prime-factors/prime-factors.js
@@ -1,8 +1,23 @@
-var primeFactors = function(input){
- this.input = input;
-};
+var primeFactors = function(){};
 
-primeFactors.prototype.for = function(){
+primeFactors.prototype.for = function(input){
+    var value = [];
+   var i =2;
+   while(input != 1){
+       if (input <= i){
+            value.push(input);
+            return value;
+       } else{
+                if(input % i === 0){
+                input = input/i;
+                value.push(i);
+                i = 2;
+                } else {
+                    i++;
+                }
+       }
+    } 
+   return value;
 
 };
 var a = new primeFactors();
