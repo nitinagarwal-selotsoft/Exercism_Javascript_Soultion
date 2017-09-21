29721fd6f15d299c1fd82d87229439221e92ac8f
nitinagarwal-selotsoft <nitina@selotsoft.com>
2017-09-21 12:38:53 +0530

    21-09-2017

--- binary/binary.js
+++ binary/binary.js
@@ -1,7 +1,17 @@
 var Binary = function(input){
     this.input = input;
 };
+//function for converting binary number to decimal number
+function bin2dec(num){
+    return num.split('').reverse().reduce(function(x, y, i){
+      return (y === '1') ? x + Math.pow(2, i) : x;
+    }, 0);
+  }
 
 Binary.prototype.toDecimal = function(){
-    
-};
+if(!this.input.match(/^[0-1]*$/)){
+     return 0;
+} else return bin2dec(this.input);
+    };
+
+module.exports = Binary;
