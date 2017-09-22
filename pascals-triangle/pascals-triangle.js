bec39efce971425ce7c069a8408d49b14705079b
nitinagarwal-selotsoft <nitina@selotsoft.com>
2017-09-22 19:35:27 +0530

    22-09-2017

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -18,28 +18,50 @@ var Triangle = function(input){
         }
     } **/
 
-     for(var i=0;i<n;i++){
+    /** 
           if(n == 1){
-                    this.rows[i] = [1];
-                    break;
+                    this.rows[i] = [];
+                    this.rows[i][i] = 1;
+                
                 } else if (n == 2){
-                    this.rows[i] = [1];
-                    this.rows[i++] = [ 1 , 1 ];
-                    break;
+                    this.rows[i] = [];
+                    this.rows[i][i] = 1;
+                    this.rows[i++] = [];
+                    this.rows[i++][i] = 1;
+                    this.rows[i++][i++] = 1;
+
+                
                 } else {
-                    this.rows[i] = [1];
-                    this.rows[i++] = [ 1 , 1 ];
-                    i = i+2;
-                    for(var j=1;j<=i+1;j++){
+                    this.rows[i] = [];
+                    this.rows[i][i] = 1;
+                    this.rows[i++] = [];
+                    this.rows[i++][i] = 1;
+                    this.rows[i++][i++] = 1;
+                
+                for(var i=2;i<n;i++){
+                    arr[i] = [];
+                    arr[i] [0] = 1;
+                    for(var j=1;j< i;j++){
                     
-                            this.rows[i][j-1] = this.undef(this.rows[i-1][j-1]) + this.undef(this.rows[i-1][j-2]);
+                            this.rows[i][j] = this.undef(this.rows[i-1][j-1]) + this.undef(this.rows[i-1][j]);
                         }
+                        
+            };
+        }**/
+
+        
+        for(var row = 0; row < n; row++ ){
+            this.rows[row] = [];
+            for(var col = 0;col < row+1; col++){
+                if(col === 0 || col === row){
+                    this.rows[row][col] = 1;
+                } else{
+                    
+                    this.rows[row][col] = this.undef(this.rows[row-1][col-1]) + this.undef(this.rows[row-1][col]); 
+                }
                 
             }
         }
-
-        
-
            
 };
 
